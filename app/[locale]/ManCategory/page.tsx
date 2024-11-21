d7cd7d8bc7b698fe8dc1baa7de429887083000b0
andriadze66 <giorgiandriadze814@gmail.com>
2024-11-21 20:22:31 +0400

    ტესტ

--- app/[locale]/ManCategory/page.tsx
+++ app/[locale]/ManCategory/page.tsx
@@ -1,6 +1,7 @@
 import { supabase } from "../lib/supabaseClient";
 import NewCard from '../components/NewCard';
 import NewHeader from '../components/Header/NewHeader';
+import Image from "next/image";
 async function GetManCategory() {
     try {
         let { data: products, error } = await supabase
@@ -21,11 +22,46 @@ export default async function ManCategoryPage() {
     return (
             <>
                 <NewHeader></NewHeader>
-
                 {
-                   manProduct && manProduct.map((product) => (
-                        <NewCard key={product.id} product={product}></NewCard>
-                    ))
+                    <div  className="TSlider-Wrapper">
+                        <div className="TSlider-Container">
+                        {manProduct && manProduct.map((item) => {
+                            return (
+                            <div key={`product-${item.product_ID}`} className="Item">
+                                <div style={{ position: "relative" }}>
+                                {item?.img &&
+                                    Object.entries(item.img).map(([_, imgArray], index) => {
+                                    if (index === 0 && Array.isArray(imgArray)) {
+                                        return (
+                                        <div
+                                            key={`image-group-${item.product_ID}-${index}`}>
+                                            <Image
+                                                key={`image-1-${item.product_ID}-${index}`}
+                                                className="T-Img"
+                                                alt=""
+                                                height={500}
+                                                width={500}
+                                                src={imgArray[0]}
+                                                />
+                                            <Image
+                                            key={`image-2-${item.product_ID}-${index}`}
+                                            className="T-Img2"
+                                            alt=""
+                                            height={500}
+                                            width={500}
+                                            src={imgArray[2]}
+                                            />
+                                        </div>
+                                        );
+                                    }
+                                    return null;
+                                    })}
+                                </div>
+                            </div>
+                            );
+                        })}
+                        </div>
+                    </div>
                 }
                 <div>ManCategoryPage</div>
             </>
